265c1d1ed42f7a76eefd913bc69f5eb82ff0cc4c
Hieu Ho <hihho@ucdavis.edu>
2020-04-20 15:23:42 -0700

    Added mongodb

--- database/index.js
+++ database/index.js
@@ -3,6 +3,10 @@ mongoose.connect('mongodb://localhost/fetcher');
 
 let repoSchema = mongoose.Schema({
   // TODO: your schema here!
+  username: String,
+  repoName: String,
+  repoID: Number,
+  forks: Number
 });
 
 let Repo = mongoose.model('Repo', repoSchema);
