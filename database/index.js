34368bd110feebde6ec86005bdad439365ea7fc0
Hieu Ho <hihho@ucdavis.edu>
2020-04-20 16:50:30 -0700

    Add data to db

--- database/index.js
+++ database/index.js
@@ -5,16 +5,27 @@ let repoSchema = mongoose.Schema({
   // TODO: your schema here!
   username: String,
   repoName: String,
-  repoID: Number,
+  repoID: {type: Number, unique: true},
   forks: Number
 });
 
 let Repo = mongoose.model('Repo', repoSchema);
 
-let save = (/* TODO */) => {
+let save = (repos) => {
   // TODO: Your code here
   // This function should save a repo or repos to
   // the MongoDB
+  let myRepo = new Repo({
+    username: repos.owner.login,
+    repoName: repos.name,
+    repoID: repos.id,
+    forks: repos.forks
+  })
+  .save((err) => {
+    if (err) {
+      console.error('SAVE ERROR!', err)
+    }
+  })
 }
 
 module.exports.save = save;
