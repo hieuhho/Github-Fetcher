22b00a93f414d0190cfdc540ef9399cf99de4148
Hieu Ho <hihho@ucdavis.edu>
2020-04-21 15:37:58 -0700

    rearranged

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -51,8 +51,8 @@ class App extends React.Component {
   render () {
     return (<div>
       <h1>Github Fetcher</h1>
-      <RepoList repos={this.state.repos}/>
       <Search handleSearch={this.search}/>
+      <RepoList repos={this.state.repos}/>
     </div>)
   }
 }
